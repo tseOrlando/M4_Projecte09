4dad0fa451dd679f6097e5ea1c24c6962f35dff6
unknown <taraujo20@jaumebalmes.net>
2023-01-18 20:00:00 +0100

    tese ex

---
+++ conc.py
@@ -0,0 +1,11 @@
+#Imprimir el resultat de la suma de les dues variables (int)
+
+a = 10
+b = 20
+
+print(a + b)
+
+#Imprimir el resultat de la suma de les dues variables (int) dintre d'un text (string)
+
+result = a + b
+print ("result : " + str(result))
---
+++ list.py
@@ -0,0 +1,31 @@
+
+
+
+areas = ["cuina", 7.88, "menjador", 13.0, "terrassa", 20.34, "lavabo", 6.55, "habitació1", 7.98, "habitació2", 12, "rebedor", 4.23]
+
+#Imprimir el segon element
+print(areas[1])
+
+#Imprimir l’últim element
+print(areas[-1])
+
+#Imprimir l’àrea de la terrassa
+print(areas[5])
+
+#Imprimir del primer element al tercer element
+print(areas[0:3])
+
+#Imprimir del tercer element a l’últim
+print(areas[2:])
+
+#Imprimir el total de l'àrea de les dues habitacions
+print(areas[9] + areas[11])
+
+#Modificar l’àrea del lavabo i imprimir la nova list area
+areas[7] = 1
+print(areas[7])
+
+#Afegir l'àrea de “pati interior” i 2.78 a les últimes posicions. Imprimir la nova list area.
+
+#no hay pati interior
+
